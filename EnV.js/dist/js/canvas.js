37ef0713dbadb7645678684af0057172176e36cc
misaki <275684857@qq.com>
2018-11-14 17:04:00 +0800

    none

--- EnV.js/dist/js/canvas.js
+++ EnV.js/dist/js/canvas.js
@@ -1,11 +1,13 @@
+function renderCanvas(id) {
+    let c = document.getElementById(id);
+    var ctx = c.getContext("2d");
+    c.width = window.screen.availWidth;
+    c.height = window.screen.availHeight;
+    ctx.rect(0, 0, c.width, c.height);
+    ctx.fillStyle = "#aaa";
+    ctx.fill();
+}
 
-
-    export default function renderCanvas(id) {
-        let c = document.getElementById(id);
-        var ctx = c.getContext("2d");
-        c.width = window.screen.availWidth;
-        c.height = window.screen.availHeight;
-        ctx.rect(0, 0, c.width, c.height);
-        ctx.fillStyle = "#aaa";
-        ctx.fill();
-    }
+export default {
+    renderCanvas
+}
